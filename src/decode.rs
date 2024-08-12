7dde2293fac328ffbd43ea7e6e8d26cf8ba3026d
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-12 14:00:28 +0200

    feat: add decode helper for register + operand instructions

--- src/decode.rs
+++ src/decode.rs
@@ -6,6 +6,14 @@ pub const DECODE_TABLE: DecodeTable = phf_map! {
     "stop" => Processor::decode_stop,
 };
 
+fn get_register_and_operand_str(mut iter: SplitWhitespace) -> Result<(&str, &str), DecodeError> {
+    let (Some(s_register), Some(s_operand)) = (iter.next(), iter.next()) else {
+        return Err(DecodeError::IncompleteInstruction);
+    };
+
+    Ok((s_register, s_operand))
+}
+
 impl Processor {
     fn decode_stop(&mut self, _iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
         Ok(Instruction::Stop)
