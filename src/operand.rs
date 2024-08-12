33726310e4067ac18f609b3e3d39d84fc8773960
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-12 13:53:29 +0200

    refactor: apply new error structure to operand

--- src/operand.rs
+++ src/operand.rs
@@ -14,54 +14,54 @@ pub enum Operand<Size> {
     Immediate(Size),
 }
 
-impl<'a> TryFrom<&'a str> for Operand<Byte> {
-    type Error = ParseError<'a>;
+impl TryFrom<&str> for Operand<Byte> {
+    type Error = DecodeError;
 
-    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
         match (s.parse::<Byte>(), Register::try_from(s)) {
             (Ok(_), Ok(_)) => unreachable!("impossible to parse as both number and register"),
             (Ok(num), Err(_)) => Ok(Operand::Immediate(num)),
             (Err(_), Ok(reg)) => Ok(Operand::Register(reg)),
-            (Err(_), Err(_)) => Err(ParseError::InvalidOperand(s)),
+            (Err(_), Err(_)) => Err(DecodeError::InvalidOperand(s.to_string())),
         }
     }
 }
 
-impl<'a> TryFrom<&'a str> for Operand<Quarter> {
-    type Error = ParseError<'a>;
+impl TryFrom<&str> for Operand<Quarter> {
+    type Error = DecodeError;
 
-    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
         match (s.parse::<Quarter>(), Register::try_from(s)) {
             (Ok(_), Ok(_)) => unreachable!("impossible to parse as both number and register"),
             (Ok(num), Err(_)) => Ok(Operand::Immediate(num)),
             (Err(_), Ok(reg)) => Ok(Operand::Register(reg)),
-            (Err(_), Err(_)) => Err(ParseError::InvalidOperand(s)),
+            (Err(_), Err(_)) => Err(DecodeError::InvalidOperand(s.to_string())),
         }
     }
 }
 
-impl<'a> TryFrom<&'a str> for Operand<Half> {
-    type Error = ParseError<'a>;
+impl TryFrom<&str> for Operand<Half> {
+    type Error = DecodeError;
 
-    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
         match (s.parse::<Half>(), Register::try_from(s)) {
             (Ok(_), Ok(_)) => unreachable!("impossible to parse as both number and register"),
             (Ok(num), Err(_)) => Ok(Operand::Immediate(num)),
             (Err(_), Ok(reg)) => Ok(Operand::Register(reg)),
-            (Err(_), Err(_)) => Err(ParseError::InvalidOperand(s)),
+            (Err(_), Err(_)) => Err(DecodeError::InvalidOperand(s.to_string())),
         }
     }
 }
 
-impl<'a> TryFrom<&'a str> for Operand<Word> {
-    type Error = ParseError<'a>;
+impl TryFrom<&str> for Operand<Word> {
+    type Error = DecodeError;
 
-    fn try_from(s: &'a str) -> Result<Self, Self::Error> {
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
         match (s.parse::<Word>(), Register::try_from(s)) {
             (Ok(_), Ok(_)) => unreachable!("impossible to parse as both number and register"),
             (Ok(num), Err(_)) => Ok(Operand::Immediate(num)),
             (Err(_), Ok(reg)) => Ok(Operand::Register(reg)),
-            (Err(_), Err(_)) => Err(ParseError::InvalidOperand(s)),
+            (Err(_), Err(_)) => Err(DecodeError::InvalidOperand(s.to_string())),
         }
     }
 }
