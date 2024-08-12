6dc025bb2f6bf7473b07c2a7f4e1fe52d34da8c6
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-12 14:02:05 +0200

    feat: add decode function for set byte instruction

--- src/decode.rs
+++ src/decode.rs
@@ -4,6 +4,7 @@ use crate::{constant::DecodeTable, error::DecodeError, instruction::Instruction,
 
 pub const DECODE_TABLE: DecodeTable = phf_map! {
     "stop" => Processor::decode_stop,
+    "setb" => Processor::decode_set_byte,
 };
 
 fn get_register_and_operand_str(mut iter: SplitWhitespace) -> Result<(&str, &str), DecodeError> {
@@ -17,5 +18,17 @@ fn get_register_and_operand_str(mut iter: SplitWhitespace) -> Result<(&str, &str
 impl Processor {
     fn decode_stop(&mut self, _iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
         Ok(Instruction::Stop)
+    }
+
+    fn decode_set_byte(&mut self, iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
+        let (s_register, s_operand) = get_register_and_operand_str(iter)?;
+
+        let register = Register::try_from(s_register)?;
+        let operand = Operand::try_from(s_operand)?;
+
+        self.registers[Register::P1] = register.as_word();
+        self.registers[Register::P2] = operand.byte_val_as_word(&self.registers);
+
+        Ok(Instruction::SetByte)
     }
 }
