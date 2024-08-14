2ec3d0f495ed0e73d143177439ec5bb35ea41bbb
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-15 00:49:12 +0200

    test: add test cases for execute_sub_quarter

--- src/execute.rs
+++ src/execute.rs
@@ -337,3 +337,36 @@ mod execute_sub_byte {
         assert!(!p.flags.sign);
     }
 }
+
+#[cfg(test)]
+mod execute_sub_quarter {
+    use crate::{constant::{Quarter, Word}, register::Register, Processor};
+
+    #[test]
+    fn sub_causes_overflow() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Quarter::MIN as Word;
+        p.registers[Register::P1] = Register::A.as_word();
+        p.registers[Register::P2] = 1;
+        p.execute_sub_quarter().unwrap();
+        
+        assert_eq!(p.registers[Register::A], Quarter::MAX as Word);
+        assert!(!p.flags.zero);
+        assert!(p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn sub_does_not_cause_overflow() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Quarter::MAX as Word;
+        p.registers[Register::P1] = Register::A.as_word();
+        p.registers[Register::P2] = Quarter::MAX as Word;
+        p.execute_sub_quarter().unwrap();
+        
+        assert_eq!(p.registers[Register::A], 0);
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+}
