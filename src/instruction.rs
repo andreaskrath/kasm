8dd3c761df74aa460536f03451d6d001a072be39
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-17 02:07:11 +0200

    refactor: add decode helpers

--- src/instruction.rs
+++ src/instruction.rs
@@ -67,167 +67,167 @@ fn get_reg_and_operand_str(mut iter: SplitWhitespace) -> Result<(&str, &str), De
     Ok((s_register, s_operand))
 }
 
+fn decode_reg_and_byte_operand(
+    iter: SplitWhitespace,
+) -> Result<(Register, Operand<Byte>), DecodeError> {
+    let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
+    let register = Register::try_from(s_register)?;
+    let operand = Operand::try_from(s_operand)?;
+
+    Ok((register, operand))
+}
+
+fn decode_reg_and_quarter_operand(
+    iter: SplitWhitespace,
+) -> Result<(Register, Operand<Quarter>), DecodeError> {
+    let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
+    let register = Register::try_from(s_register)?;
+    let operand = Operand::try_from(s_operand)?;
+
+    Ok((register, operand))
+}
+
+fn decode_reg_and_half_operand(
+    iter: SplitWhitespace,
+) -> Result<(Register, Operand<Half>), DecodeError> {
+    let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
+    let register = Register::try_from(s_register)?;
+    let operand = Operand::try_from(s_operand)?;
+
+    Ok((register, operand))
+}
+
+fn decode_reg_and_word_operand(
+    iter: SplitWhitespace,
+) -> Result<(Register, Operand<Word>), DecodeError> {
+    let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
+    let register = Register::try_from(s_register)?;
+    let operand = Operand::try_from(s_operand)?;
+
+    Ok((register, operand))
+}
+
 impl Instruction {
     pub fn decode_stop(_iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
         Ok(Instruction::Stop)
     }
 
     pub fn decode_set_byte(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_byte_operand(iter)?;
 
         Ok(Instruction::SetByte(register, operand))
     }
 
     pub fn decode_set_quarter(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_quarter_operand(iter)?;
 
         Ok(Instruction::SetQuarter(register, operand))
     }
 
     pub fn decode_set_half(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_half_operand(iter)?;
 
         Ok(Instruction::SetHalf(register, operand))
     }
 
     pub fn decode_set_word(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_word_operand(iter)?;
 
         Ok(Instruction::SetWord(register, operand))
     }
 
     pub fn decode_add_byte(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_byte_operand(iter)?;
 
         Ok(Instruction::AddByte(register, operand))
     }
 
     pub fn decode_add_quarter(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_quarter_operand(iter)?;
 
         Ok(Instruction::AddQuarter(register, operand))
     }
 
     pub fn decode_add_half(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_half_operand(iter)?;
 
         Ok(Instruction::AddHalf(register, operand))
     }
 
     pub fn decode_add_word(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_word_operand(iter)?;
 
         Ok(Instruction::AddWord(register, operand))
     }
 
     pub fn decode_sub_byte(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_byte_operand(iter)?;
 
         Ok(Instruction::SubByte(register, operand))
     }
 
     pub fn decode_sub_quarter(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_quarter_operand(iter)?;
 
         Ok(Instruction::SubQuarter(register, operand))
     }
 
     pub fn decode_sub_half(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_half_operand(iter)?;
 
         Ok(Instruction::SubHalf(register, operand))
     }
 
     pub fn decode_sub_word(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_word_operand(iter)?;
 
         Ok(Instruction::SubWord(register, operand))
     }
 
     pub fn decode_mul_byte(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_byte_operand(iter)?;
 
         Ok(Instruction::MulByte(register, operand))
     }
 
     pub fn decode_mul_quarter(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_quarter_operand(iter)?;
 
         Ok(Instruction::MulQuarter(register, operand))
     }
 
     pub fn decode_mul_half(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_half_operand(iter)?;
 
         Ok(Instruction::MulHalf(register, operand))
     }
 
     pub fn decode_mul_word(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_word_operand(iter)?;
 
         Ok(Instruction::MulWord(register, operand))
     }
 
     pub fn decode_div_byte(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_byte_operand(iter)?;
 
         Ok(Instruction::DivByte(register, operand))
     }
 
     pub fn decode_div_quarter(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_quarter_operand(iter)?;
 
         Ok(Instruction::DivQuarter(register, operand))
     }
 
     pub fn decode_div_half(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_half_operand(iter)?;
 
         Ok(Instruction::DivHalf(register, operand))
     }
 
     pub fn decode_div_word(iter: SplitWhitespace) -> Result<Instruction, DecodeError> {
-        let (s_register, s_operand) = get_reg_and_operand_str(iter)?;
-        let register = Register::try_from(s_register)?;
-        let operand = Operand::try_from(s_operand)?;
+        let (register, operand) = decode_reg_and_word_operand(iter)?;
 
         Ok(Instruction::DivWord(register, operand))
     }
