e36e39b59eaf584f297afd0384542eb203c128d9
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-23 13:35:44 +0200

    refactor: remove unused methods for RegisterOperations

--- src/registers.rs
+++ src/registers.rs
@@ -8,9 +8,6 @@ pub trait RegisterOperations {
     fn get_reg_val_as_quarter(&self, register: Register) -> Quarter;
     fn get_reg_val_as_half(&self, register: Register) -> Half;
     fn get_reg_val(&self, register: Register) -> Word;
-    fn get_reg_byte_val_as_word(&self, register: Register) -> Word;
-    fn get_reg_quarter_val_as_word(&self, register: Register) -> Word;
-    fn get_reg_half_val_as_word(&self, register: Register) -> Word;
 }
 
 pub type Registers = [Word; Register::VARIANT_COUNT];
@@ -34,21 +31,5 @@ impl RegisterOperations for Registers {
 
     fn get_reg_val(&self, register: Register) -> Word {
         self[register]
-    }
-
-    fn get_reg_byte_val_as_word(&self, register: Register) -> Word {
-        self[register].to_le_bytes()[0] as Word
-    }
-
-    fn get_reg_quarter_val_as_word(&self, register: Register) -> Word {
-        let mut bytes = [0; 2];
-        bytes.copy_from_slice(&self[register].to_le_bytes()[0..2]);
-        Quarter::from_le_bytes(bytes) as Word
-    }
-
-    fn get_reg_half_val_as_word(&self, register: Register) -> Word {
-        let mut bytes = [0; 4];
-        bytes.copy_from_slice(&self[register].to_le_bytes()[0..4]);
-        Half::from_le_bytes(bytes) as Word
     }
 }
