0d3a40cc8d188f6057c312c823237dabb1847726
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-24 13:16:13 +0200

    refactor: make sub execution generic

--- src/execute/subtraction.rs
+++ src/execute/subtraction.rs
@@ -1,56 +1,28 @@
 use crate::{
-    constant::{Byte, Half, Quarter, Word},
-    instruction::Subtraction,
-    operand::Operand,
-    register::Register,
-    registers::RegisterOperations,
-    Processor,
+    instruction::Subtraction, operand::Operand, register::Register, registers::RegisterOperations,
+    utils::Arithmetic, Processor,
 };
 
 impl Processor {
     pub fn sub(&mut self, instruction: Subtraction) {
         match instruction {
-            Subtraction::Byte(r, o) => self.sub_byte(r, o),
-            Subtraction::Quarter(r, o) => self.sub_quarter(r, o),
-            Subtraction::Half(r, o) => self.sub_half(r, o),
-            Subtraction::Word(r, o) => self.sub_word(r, o),
+            Subtraction::Byte(r, o) => self.sub_value(r, o),
+            Subtraction::Quarter(r, o) => self.sub_value(r, o),
+            Subtraction::Half(r, o) => self.sub_value(r, o),
+            Subtraction::Word(r, o) => self.sub_value(r, o),
         }
     }
 
-    fn sub_byte(&mut self, register: Register, operand: Operand<Byte>) {
-        let a = self.registers.get::<Byte>(register);
+    fn sub_value<T>(&mut self, register: Register, operand: Operand<T>)
+    where
+        T: Arithmetic,
+    {
+        let a = self.registers.get::<T>(register);
         let b = self.get_operand_value(operand);
 
-        let (result, overflow) = a.overflowing_sub(b);
+        let (result, overflow) = a.overflow_sub(b);
         self.flags.set(result, overflow);
-        self.registers[register] = result as Word;
-    }
-
-    fn sub_quarter(&mut self, register: Register, operand: Operand<Quarter>) {
-        let a = self.registers.get::<Quarter>(register);
-        let b = self.get_operand_value(operand);
-
-        let (result, overflow) = a.overflowing_sub(b);
-        self.flags.set(result, overflow);
-        self.registers[register] = result as Word;
-    }
-
-    fn sub_half(&mut self, register: Register, operand: Operand<Half>) {
-        let a = self.registers.get::<Half>(register);
-        let b = self.get_operand_value(operand);
-
-        let (result, overflow) = a.overflowing_sub(b);
-        self.flags.set(result, overflow);
-        self.registers[register] = result as Word;
-    }
-
-    fn sub_word(&mut self, register: Register, operand: Operand<Word>) {
-        let a = self.registers.get::<Word>(register);
-        let b = self.get_operand_value(operand);
-
-        let (result, overflow) = a.overflowing_sub(b);
-        self.flags.set(result, overflow);
-        self.registers[register] = result;
+        self.registers[register] = result.to_word();
     }
 }
 
@@ -69,7 +41,7 @@ mod byte {
         p.registers[Register::A] = Byte::MIN as Word;
         let expected = Byte::MAX as Word;
 
-        p.sub_byte(Register::A, Operand::Immediate(1));
+        p.sub_value(Register::A, Operand::Immediate(1u8));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(p.flags.overflow);
@@ -83,7 +55,7 @@ mod byte {
         p.registers[Register::A] = Byte::MAX as Word;
         let expected = Byte::MAX as Word - 1;
 
-        p.sub_byte(Register::A, Operand::Immediate(1));
+        p.sub_value(Register::A, Operand::Immediate(1u8));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -98,7 +70,7 @@ mod byte {
         p.registers[Register::B] = 2;
         let expected = 0;
 
-        p.sub_byte(Register::A, Operand::Register(Register::B));
+        p.sub_value(Register::A, Operand::<Byte>::Register(Register::B));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -112,7 +84,7 @@ mod byte {
         p.registers[Register::A] = 2;
         let expected = 0;
 
-        p.sub_byte(Register::A, Operand::Register(Register::A));
+        p.sub_value(Register::A, Operand::<Byte>::Register(Register::A));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -136,7 +108,7 @@ mod quarter {
         p.registers[Register::A] = Quarter::MIN as Word;
         let expected = Quarter::MAX as Word;
 
-        p.sub_quarter(Register::A, Operand::Immediate(1));
+        p.sub_value(Register::A, Operand::Immediate(1u16));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(p.flags.overflow);
@@ -150,7 +122,7 @@ mod quarter {
         p.registers[Register::A] = Quarter::MAX as Word;
         let expected = Quarter::MAX as Word - 1;
 
-        p.sub_quarter(Register::A, Operand::Immediate(1));
+        p.sub_value(Register::A, Operand::Immediate(1u16));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -165,7 +137,7 @@ mod quarter {
         p.registers[Register::B] = 2;
         let expected = 0;
 
-        p.sub_quarter(Register::A, Operand::Register(Register::B));
+        p.sub_value(Register::A, Operand::<Quarter>::Register(Register::B));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -179,7 +151,7 @@ mod quarter {
         p.registers[Register::A] = 2;
         let expected = 0;
 
-        p.sub_quarter(Register::A, Operand::Register(Register::A));
+        p.sub_value(Register::A, Operand::<Quarter>::Register(Register::A));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -203,7 +175,7 @@ mod half {
         p.registers[Register::A] = Half::MIN as Word;
         let expected = Half::MAX as Word;
 
-        p.sub_half(Register::A, Operand::Immediate(1));
+        p.sub_value(Register::A, Operand::Immediate(1u32));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(p.flags.overflow);
@@ -217,7 +189,7 @@ mod half {
         p.registers[Register::A] = Half::MAX as Word;
         let expected = Half::MAX as Word - 1;
 
-        p.sub_half(Register::A, Operand::Immediate(1));
+        p.sub_value(Register::A, Operand::Immediate(1u32));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -232,7 +204,7 @@ mod half {
         p.registers[Register::B] = 2;
         let expected = 0;
 
-        p.sub_half(Register::A, Operand::Register(Register::B));
+        p.sub_value(Register::A, Operand::<Half>::Register(Register::B));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -246,7 +218,7 @@ mod half {
         p.registers[Register::A] = 2;
         let expected = 0;
 
-        p.sub_half(Register::A, Operand::Register(Register::A));
+        p.sub_value(Register::A, Operand::<Half>::Register(Register::A));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -265,7 +237,7 @@ mod word {
         p.registers[Register::A] = Word::MIN;
         let expected = Word::MAX;
 
-        p.sub_word(Register::A, Operand::Immediate(1));
+        p.sub_value(Register::A, Operand::Immediate(1u64));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(p.flags.overflow);
@@ -279,7 +251,7 @@ mod word {
         p.registers[Register::A] = Word::MAX;
         let expected = Word::MAX - 1;
 
-        p.sub_word(Register::A, Operand::Immediate(1));
+        p.sub_value(Register::A, Operand::Immediate(1u64));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -294,7 +266,7 @@ mod word {
         p.registers[Register::B] = 2;
         let expected = 0;
 
-        p.sub_word(Register::A, Operand::Register(Register::B));
+        p.sub_value(Register::A, Operand::<Word>::Register(Register::B));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
@@ -308,7 +280,7 @@ mod word {
         p.registers[Register::A] = 2;
         let expected = 0;
 
-        p.sub_word(Register::A, Operand::Register(Register::A));
+        p.sub_value(Register::A, Operand::<Word>::Register(Register::A));
 
         assert_eq!(p.registers[Register::A], expected);
         assert!(!p.flags.overflow);
--- src/utils/overflow.rs
+++ src/utils/overflow.rs
@@ -4,11 +4,19 @@ pub trait Overflow {
     fn overflow_add(self, rhs: Self) -> (Self, bool)
     where
         Self: Sized;
+
+    fn overflow_sub(self, rhs: Self) -> (Self, bool)
+    where
+        Self: Sized;
 }
 
 impl Overflow for Byte {
     fn overflow_add(self, rhs: Self) -> (Self, bool) {
         self.overflowing_add(rhs)
+    }
+
+    fn overflow_sub(self, rhs: Self) -> (Self, bool) {
+        self.overflowing_sub(rhs)
     }
 }
 
@@ -16,16 +24,28 @@ impl Overflow for Quarter {
     fn overflow_add(self, rhs: Self) -> (Self, bool) {
         self.overflowing_add(rhs)
     }
+
+    fn overflow_sub(self, rhs: Self) -> (Self, bool) {
+        self.overflowing_sub(rhs)
+    }
 }
 
 impl Overflow for Half {
     fn overflow_add(self, rhs: Self) -> (Self, bool) {
         self.overflowing_add(rhs)
     }
+
+    fn overflow_sub(self, rhs: Self) -> (Self, bool) {
+        self.overflowing_sub(rhs)
+    }
 }
 
 impl Overflow for Word {
     fn overflow_add(self, rhs: Self) -> (Self, bool) {
         self.overflowing_add(rhs)
+    }
+
+    fn overflow_sub(self, rhs: Self) -> (Self, bool) {
+        self.overflowing_sub(rhs)
     }
 }
