f1cd45709bf6b8f666e765da961ec54e49c62b55
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-26 00:27:51 +0200

    refactor: add test attribute to Writer method
    prevent unused method warning, and only exposes method during tests

--- src/utils/writer.rs
+++ src/utils/writer.rs
@@ -1,16 +1,19 @@
 use std::io::{Stdout, Write};
 
 pub trait Writer: Write {
+    #[cfg(test)]
     fn get_buffer(&self) -> Option<String>;
 }
 
 impl Writer for Stdout {
+    #[cfg(test)]
     fn get_buffer(&self) -> Option<String> {
         None
     }
 }
 
 impl Writer for Vec<u8> {
+    #[cfg(test)]
     fn get_buffer(&self) -> Option<String> {
         Some(String::from_utf8(self.clone()).unwrap())
     }
