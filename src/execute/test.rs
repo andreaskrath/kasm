ba753a7d6a5d4d08ca65a8ff625b05b7d75079e1
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-25 18:41:25 +0200

    test: add additional test cases for execute test instructions

--- src/execute/test.rs
+++ src/execute/test.rs
@@ -31,11 +31,10 @@ mod byte {
     };
 
     #[test]
-    fn no_bits_in_common() {
+    fn no_bits_in_common_immediate() {
         let mut p = Processor::new().unwrap();
-        p.registers[Register::A] = Byte::MAX as Word;
 
-        p.test_value::<Byte>(Operand::Register(Register::A), Operand::Immediate(0));
+        p.test_value(Operand::Immediate(Byte::MAX), Operand::Immediate(0));
 
         assert!(p.flags.zero);
         assert!(!p.flags.overflow);
@@ -43,7 +42,63 @@ mod byte {
     }
 
     #[test]
-    fn all_bits_in_common() {
+    fn all_bits_in_common_immediate() {
+        let mut p = Processor::new().unwrap();
+
+        p.test_value(Operand::Immediate(Byte::MAX), Operand::Immediate(Byte::MAX));
+
+        assert!(!p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn no_bits_in_common_register() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Byte::MAX as Word;
+
+        p.test_value::<Byte>(
+            Operand::Register(Register::A),
+            Operand::Register(Register::B),
+        );
+
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+
+    #[test]
+    fn all_bits_in_common_register() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Byte::MAX as Word;
+        p.registers[Register::B] = Byte::MAX as Word;
+
+        p.test_value::<Byte>(
+            Operand::Register(Register::A),
+            Operand::Register(Register::B),
+        );
+
+        assert!(!p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn no_bits_in_common_mix() {
+        let mut p = Processor::new().unwrap();
+
+        p.test_value(
+            Operand::Register(Register::A),
+            Operand::Immediate(Byte::MAX),
+        );
+
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+
+    #[test]
+    fn all_bits_in_common_mix() {
         let mut p = Processor::new().unwrap();
         p.registers[Register::A] = Byte::MAX as Word;
 
@@ -68,11 +123,10 @@ mod quarter {
     };
 
     #[test]
-    fn no_bits_in_common() {
+    fn no_bits_in_common_immediate() {
         let mut p = Processor::new().unwrap();
-        p.registers[Register::A] = Quarter::MAX as Word;
 
-        p.test_value::<Quarter>(Operand::Register(Register::A), Operand::Immediate(0));
+        p.test_value(Operand::Immediate(Quarter::MAX), Operand::Immediate(0));
 
         assert!(p.flags.zero);
         assert!(!p.flags.overflow);
@@ -80,7 +134,66 @@ mod quarter {
     }
 
     #[test]
-    fn all_bits_in_common() {
+    fn all_bits_in_common_immediate() {
+        let mut p = Processor::new().unwrap();
+
+        p.test_value(
+            Operand::Immediate(Quarter::MAX),
+            Operand::Immediate(Quarter::MAX),
+        );
+
+        assert!(!p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn no_bits_in_common_register() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Quarter::MAX as Word;
+
+        p.test_value::<Quarter>(
+            Operand::Register(Register::A),
+            Operand::Register(Register::B),
+        );
+
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+
+    #[test]
+    fn all_bits_in_common_register() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Quarter::MAX as Word;
+        p.registers[Register::B] = Quarter::MAX as Word;
+
+        p.test_value::<Quarter>(
+            Operand::Register(Register::A),
+            Operand::Register(Register::B),
+        );
+
+        assert!(!p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn no_bits_in_common_mix() {
+        let mut p = Processor::new().unwrap();
+
+        p.test_value(
+            Operand::Register(Register::A),
+            Operand::Immediate(Quarter::MAX),
+        );
+
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+
+    #[test]
+    fn all_bits_in_common_mix() {
         let mut p = Processor::new().unwrap();
         p.registers[Register::A] = Quarter::MAX as Word;
 
@@ -105,11 +218,10 @@ mod half {
     };
 
     #[test]
-    fn no_bits_in_common() {
+    fn no_bits_in_common_immediate() {
         let mut p = Processor::new().unwrap();
-        p.registers[Register::A] = Half::MAX as Word;
 
-        p.test_value::<Half>(Operand::Register(Register::A), Operand::Immediate(0));
+        p.test_value(Operand::Immediate(Half::MAX), Operand::Immediate(0));
 
         assert!(p.flags.zero);
         assert!(!p.flags.overflow);
@@ -117,7 +229,63 @@ mod half {
     }
 
     #[test]
-    fn all_bits_in_common() {
+    fn all_bits_in_common_immediate() {
+        let mut p = Processor::new().unwrap();
+
+        p.test_value(Operand::Immediate(Half::MAX), Operand::Immediate(Half::MAX));
+
+        assert!(!p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn no_bits_in_common_register() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Half::MAX as Word;
+
+        p.test_value::<Half>(
+            Operand::Register(Register::A),
+            Operand::Register(Register::B),
+        );
+
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+
+    #[test]
+    fn all_bits_in_common_register() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Half::MAX as Word;
+        p.registers[Register::B] = Half::MAX as Word;
+
+        p.test_value::<Half>(
+            Operand::Register(Register::A),
+            Operand::Register(Register::B),
+        );
+
+        assert!(!p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn no_bits_in_common_mix() {
+        let mut p = Processor::new().unwrap();
+
+        p.test_value(
+            Operand::Register(Register::A),
+            Operand::Immediate(Half::MAX),
+        );
+
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+
+    #[test]
+    fn all_bits_in_common_mix() {
         let mut p = Processor::new().unwrap();
         p.registers[Register::A] = Half::MAX as Word;
 
@@ -137,11 +305,10 @@ mod word {
     use crate::{constant::Word, operand::Operand, register::Register, Processor};
 
     #[test]
-    fn no_bits_in_common() {
+    fn no_bits_in_common_immediate() {
         let mut p = Processor::new().unwrap();
-        p.registers[Register::A] = Word::MAX;
 
-        p.test_value::<Word>(Operand::Register(Register::A), Operand::Immediate(0));
+        p.test_value(Operand::Immediate(Word::MAX), Operand::Immediate(0));
 
         assert!(p.flags.zero);
         assert!(!p.flags.overflow);
@@ -149,7 +316,63 @@ mod word {
     }
 
     #[test]
-    fn all_bits_in_common() {
+    fn all_bits_in_common_immediate() {
+        let mut p = Processor::new().unwrap();
+
+        p.test_value(Operand::Immediate(Word::MAX), Operand::Immediate(Word::MAX));
+
+        assert!(!p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn no_bits_in_common_register() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Word::MAX;
+
+        p.test_value::<Word>(
+            Operand::Register(Register::A),
+            Operand::Register(Register::B),
+        );
+
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+
+    #[test]
+    fn all_bits_in_common_register() {
+        let mut p = Processor::new().unwrap();
+        p.registers[Register::A] = Word::MAX;
+        p.registers[Register::B] = Word::MAX;
+
+        p.test_value::<Word>(
+            Operand::Register(Register::A),
+            Operand::Register(Register::B),
+        );
+
+        assert!(!p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(p.flags.sign);
+    }
+
+    #[test]
+    fn no_bits_in_common_mix() {
+        let mut p = Processor::new().unwrap();
+
+        p.test_value(
+            Operand::Register(Register::A),
+            Operand::Immediate(Word::MAX),
+        );
+
+        assert!(p.flags.zero);
+        assert!(!p.flags.overflow);
+        assert!(!p.flags.sign);
+    }
+
+    #[test]
+    fn all_bits_in_common_mix() {
         let mut p = Processor::new().unwrap();
         p.registers[Register::A] = Word::MAX;
 
