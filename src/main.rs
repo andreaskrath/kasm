e711ab12c8817f1284dde82ec3908068427f9c8f
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-26 13:31:08 +0200

    feat: include example program
    computes 10 first fibonacci numbers, places them on the stack and prints

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use kasm::Interpreter;
 fn main() {
     let mut p = Interpreter::default();
     // this is just an example program for now
-    let program = String::from("set ra 2500\nstp");
+    let program = include_str!("../program.kasm");
 
     if let Err(err) = p.run(program) {
         eprintln!("{}", err)
