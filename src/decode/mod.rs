2517358b82a151d5a68d551a3bbf1af6b978817e
Andreas Krath <andreas.krath+github@gmail.com>
2024-08-27 18:49:55 +0200

    test: add regression test cases for decoding xor instructions

--- src/decode/mod.rs
+++ src/decode/mod.rs
@@ -3649,3 +3649,415 @@ mod or_regression {
         }
     }
 }
+
+#[cfg(test)]
+mod xor_regression {
+    mod byte {
+        use crate::{
+            error::DecodeError,
+            instruction::{Instruction, Xor},
+            operand::Operand,
+            register::Register,
+            Interpreter,
+        };
+
+        #[test]
+        fn incomplete_instruction_error_missing_first_param() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorb";
+            let expected = Err(DecodeError::IncompleteInstruction);
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn incomplete_instruction_error_missing_second_param() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorb ra";
+            let expected = Err(DecodeError::IncompleteInstruction);
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_register_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorb rx 1";
+            let expected = Err(DecodeError::InvalidRegister("rx".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_register_error_in_operand() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorb ra rx";
+            let expected = Err(DecodeError::InvalidRegister("rx".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_immediate_value_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorb ra -1";
+            let expected = Err(DecodeError::InvalidImmediateValue("-1".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_operand_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorb ra 200u8";
+            let expected = Err(DecodeError::InvalidOperand("200u8".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn second_param_register() -> Result<(), DecodeError> {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorb ra rb";
+            let expected = Instruction::Xor(Xor::Byte(Register::A, Operand::Register(Register::B)));
+
+            let actual = i.decode(instruction)?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+
+        #[test]
+        fn second_param_immediate() -> Result<(), DecodeError> {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorb ra 20";
+            let expected = Instruction::Xor(Xor::Byte(Register::A, Operand::Immediate(20)));
+
+            let actual = i.decode(instruction)?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+    }
+
+    mod quarter {
+        use crate::{
+            error::DecodeError,
+            instruction::{Instruction, Xor},
+            operand::Operand,
+            register::Register,
+            Interpreter,
+        };
+
+        #[test]
+        fn incomplete_instruction_error_missing_first_param() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorq";
+            let expected = Err(DecodeError::IncompleteInstruction);
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn incomplete_instruction_error_missing_second_param() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorq ra";
+            let expected = Err(DecodeError::IncompleteInstruction);
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_register_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorq rx 1";
+            let expected = Err(DecodeError::InvalidRegister("rx".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_register_error_in_operand() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorq ra rx";
+            let expected = Err(DecodeError::InvalidRegister("rx".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_immediate_value_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorq ra -1";
+            let expected = Err(DecodeError::InvalidImmediateValue("-1".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_operand_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorq ra 200u8";
+            let expected = Err(DecodeError::InvalidOperand("200u8".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn second_param_register() -> Result<(), DecodeError> {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorq ra rb";
+            let expected =
+                Instruction::Xor(Xor::Quarter(Register::A, Operand::Register(Register::B)));
+
+            let actual = i.decode(instruction)?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+
+        #[test]
+        fn second_param_immediate() -> Result<(), DecodeError> {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorq ra 20";
+            let expected = Instruction::Xor(Xor::Quarter(Register::A, Operand::Immediate(20)));
+
+            let actual = i.decode(instruction)?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+    }
+
+    mod half {
+        use crate::{
+            error::DecodeError,
+            instruction::{Instruction, Xor},
+            operand::Operand,
+            register::Register,
+            Interpreter,
+        };
+
+        #[test]
+        fn incomplete_instruction_error_missing_first_param() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorh";
+            let expected = Err(DecodeError::IncompleteInstruction);
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn incomplete_instruction_error_missing_second_param() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorh ra";
+            let expected = Err(DecodeError::IncompleteInstruction);
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_register_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorh rx 1";
+            let expected = Err(DecodeError::InvalidRegister("rx".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_register_error_in_operand() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorh ra rx";
+            let expected = Err(DecodeError::InvalidRegister("rx".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_immediate_value_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorh ra -1";
+            let expected = Err(DecodeError::InvalidImmediateValue("-1".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_operand_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorh ra 200u8";
+            let expected = Err(DecodeError::InvalidOperand("200u8".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn second_param_register() -> Result<(), DecodeError> {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorh ra rb";
+            let expected = Instruction::Xor(Xor::Half(Register::A, Operand::Register(Register::B)));
+
+            let actual = i.decode(instruction)?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+
+        #[test]
+        fn second_param_immediate() -> Result<(), DecodeError> {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorh ra 20";
+            let expected = Instruction::Xor(Xor::Half(Register::A, Operand::Immediate(20)));
+
+            let actual = i.decode(instruction)?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+    }
+
+    mod word {
+        use crate::{
+            error::DecodeError,
+            instruction::{Instruction, Xor},
+            operand::Operand,
+            register::Register,
+            Interpreter,
+        };
+
+        #[test]
+        fn incomplete_instruction_error_missing_first_param() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorw";
+            let expected = Err(DecodeError::IncompleteInstruction);
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn incomplete_instruction_error_missing_second_param() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorw ra";
+            let expected = Err(DecodeError::IncompleteInstruction);
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_register_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorw rx 1";
+            let expected = Err(DecodeError::InvalidRegister("rx".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_register_error_in_operand() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorw ra rx";
+            let expected = Err(DecodeError::InvalidRegister("rx".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_immediate_value_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorw ra -1";
+            let expected = Err(DecodeError::InvalidImmediateValue("-1".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn invalid_operand_error() {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorw ra 200u8";
+            let expected = Err(DecodeError::InvalidOperand("200u8".to_string()));
+
+            let actual = i.decode(instruction);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn second_param_register() -> Result<(), DecodeError> {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorw ra rb";
+            let expected = Instruction::Xor(Xor::Word(Register::A, Operand::Register(Register::B)));
+
+            let actual = i.decode(instruction)?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+
+        #[test]
+        fn second_param_immediate() -> Result<(), DecodeError> {
+            let mut i = Interpreter::new_test();
+            let instruction = "xorw ra 20";
+            let expected = Instruction::Xor(Xor::Word(Register::A, Operand::Immediate(20)));
+
+            let actual = i.decode(instruction)?;
+
+            assert_eq!(actual, expected);
+
+            Ok(())
+        }
+    }
+}
