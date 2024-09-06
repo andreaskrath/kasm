f4c9cb16cdab0f125f342b4885a395b1a34bf279
Andreas Krath <andreas.krath+github@gmail.com>
2024-09-06 15:51:58 +0200

    feat: implement ToWord for signed types

--- src/utils/to_word.rs
+++ src/utils/to_word.rs
@@ -1,4 +1,7 @@
-use crate::constant::{Byte, Half, Quarter, Word};
+use crate::constant::{
+    Byte, Half, Quarter, SignedByte, SignedHalf, SignedQuarter, SignedWord, Word,
+};
+use std::mem;
 
 pub trait ToWord {
     fn to_word(self) -> Word;
@@ -25,5 +28,62 @@ impl ToWord for Half {
 impl ToWord for Word {
     fn to_word(self) -> Word {
         self
+    }
+}
+
+impl ToWord for SignedByte {
+    fn to_word(self) -> Word {
+        #[expect(
+            unsafe_code,
+            reason = "
+                this is the only way to reinterpted the underlying bits into a new type, 
+                while also maintaining the same numerical value if reinterpretted back to the original type
+            "
+        )]
+        let byte: Byte = unsafe { mem::transmute(self) };
+        Word::from(byte)
+    }
+}
+
+impl ToWord for SignedQuarter {
+    fn to_word(self) -> Word {
+        #[expect(
+            unsafe_code,
+            reason = "
+                this is the only way to reinterpted the underlying bits into a new type, 
+                while also maintaining the same numerical value if reinterpretted back to the original type
+            "
+        )]
+        let quarter: Quarter = unsafe { mem::transmute(self) };
+        Word::from(quarter)
+    }
+}
+
+impl ToWord for SignedHalf {
+    fn to_word(self) -> Word {
+        #[expect(
+            unsafe_code,
+            reason = "
+                this is the only way to reinterpted the underlying bits into a new type, 
+                while also maintaining the same numerical value if reinterpretted back to the original type
+            "
+        )]
+        let half: Half = unsafe { mem::transmute(self) };
+        Word::from(half)
+    }
+}
+
+impl ToWord for SignedWord {
+    fn to_word(self) -> Word {
+        #[expect(
+            unsafe_code,
+            reason = "
+                this is the only way to reinterpted the underlying bits into a new type, 
+                while also maintaining the same numerical value if reinterpretted back to the original type
+            "
+        )]
+        unsafe {
+            mem::transmute(self)
+        }
     }
 }
