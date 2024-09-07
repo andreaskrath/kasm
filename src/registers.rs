11557f189c62350d539ffb8d1acf97e7bdaff96f
Andreas Krath <andreas.krath+github@gmail.com>
2024-09-07 22:07:31 +0200

    refactor: add set method to registers

--- src/registers.rs
+++ src/registers.rs
@@ -1,14 +1,23 @@
-use crate::{constant::Word, register::Register, utils::FromBytes};
+use crate::{
+    constant::Word,
+    register::Register,
+    utils::{FromBytes, ToWord},
+};
 
 pub type Registers = [Word; Register::VARIANT_COUNT];
 
 pub trait RegisterOperations {
     fn get<T: FromBytes>(&self, register: Register) -> T;
+    fn set<T: ToWord>(&mut self, register: Register, value: T);
 }
 
 impl RegisterOperations for Registers {
     fn get<T: FromBytes>(&self, register: Register) -> T {
         let slice = &self[register].to_le_bytes()[0..size_of::<T>()];
         T::from_bytes(slice)
+    }
+
+    fn set<T: ToWord>(&mut self, register: Register, value: T) {
+        let _ = self[register] = value.to_word();
     }
 }
