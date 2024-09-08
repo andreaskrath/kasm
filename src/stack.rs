d1d32fad9993839686d37f53bbc210565a55baa8
Andreas Krath <andreas.krath+github@gmail.com>
2024-09-08 20:03:37 +0200

    test: add test cases for stack methods

--- src/stack.rs
+++ src/stack.rs
@@ -109,6 +109,8 @@ mod push {
             stack::Stack,
         };
 
+        const TYPE_SIZE: usize = size_of::<Byte>();
+
         #[test]
         fn stack_overflow() {
             let mut s = Stack::new(TEST_STACK_SIZE);
@@ -121,23 +123,439 @@ mod push {
         }
 
         #[test]
-        fn no_stack_overflow() -> Result<(), ExecuteError> {
+        fn different_byte_values() -> Result<(), ExecuteError> {
             let mut s = Stack::new(TEST_STACK_SIZE);
-            let expected = Byte::MAX;
+            let value = Byte::MAX / 2;
+            let expected = value.to_le_bytes();
 
-            s.push(Byte::MAX)?;
+            s.push(value)?;
+            let actual = &s.bytes[0..TYPE_SIZE];
 
-            assert_eq!(s.bytes[0], expected);
+            assert_eq!(actual, expected);
+            assert_eq!(s.pointer, TYPE_SIZE as Word);
+
+            Ok(())
+        }
+    }
+
+    mod quarter {
+        use crate::{
+            constant::{Quarter, Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Quarter>();
+
+        #[test]
+        fn stack_overflow() {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            s.pointer = TEST_STACK_SIZE as Word;
+            let expected = Err(ExecuteError::StackOverflow);
+
+            let actual = s.push(Quarter::MAX);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = Quarter::MAX / 2;
+            let expected = value.to_le_bytes();
+
+            s.push(value)?;
+            let actual = &s.bytes[0..TYPE_SIZE];
+
+            assert_eq!(actual, expected);
+            assert_eq!(s.pointer, TYPE_SIZE as Word);
+
+            Ok(())
+        }
+    }
+
+    mod half {
+        use crate::{
+            constant::{Half, Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Half>();
+
+        #[test]
+        fn stack_overflow() {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            s.pointer = TEST_STACK_SIZE as Word;
+            let expected = Err(ExecuteError::StackOverflow);
+
+            let actual = s.push(Half::MAX);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = Half::MAX / 2;
+            let expected = value.to_le_bytes();
+
+            s.push(value)?;
+            let actual = &s.bytes[0..TYPE_SIZE];
+
+            assert_eq!(actual, expected);
+            assert_eq!(s.pointer, TYPE_SIZE as Word);
+
+            Ok(())
+        }
+    }
+
+    mod word {
+        use crate::{
+            constant::{Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Word>();
+
+        #[test]
+        fn stack_overflow() {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            s.pointer = TEST_STACK_SIZE as Word;
+            let expected = Err(ExecuteError::StackOverflow);
+
+            let actual = s.push(Word::MAX);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = Word::MAX;
+            let expected = value.to_le_bytes();
+
+            s.push(value)?;
+            let actual = &s.bytes[0..TYPE_SIZE];
+
+            assert_eq!(actual, expected);
+            assert_eq!(s.pointer, TYPE_SIZE as Word);
 
             Ok(())
         }
     }
 }
 
+#[expect(
+    clippy::indexing_slicing,
+    reason = "it is necessary to index directly in the test cases"
+)]
 #[cfg(test)]
 mod pop {
     mod byte {
+        use crate::{
+            constant::{Byte, Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Byte>();
+
         #[test]
-        fn stack_underflow() {}
+        fn stack_underflow() {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let expected = Err(ExecuteError::StackUnderflow);
+
+            let actual = s.pop::<Byte>();
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = Byte::MAX / 2;
+            s.bytes[0..TYPE_SIZE].copy_from_slice(&value.to_le_bytes());
+            s.pointer = TYPE_SIZE as Word;
+            let expected = value;
+
+            let actual = s.pop::<Byte>()?;
+
+            assert_eq!(actual, expected);
+            assert_eq!(s.pointer, 0);
+
+            Ok(())
+        }
+    }
+
+    mod quarter {
+        use crate::{
+            constant::{Quarter, Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Quarter>();
+
+        #[test]
+        fn stack_underflow() {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let expected = Err(ExecuteError::StackUnderflow);
+
+            let actual = s.pop::<Quarter>();
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = Quarter::MAX / 2;
+            s.bytes[0..TYPE_SIZE].copy_from_slice(&value.to_le_bytes());
+            s.pointer = TYPE_SIZE as Word;
+            let expected = value;
+
+            let actual = s.pop::<Quarter>()?;
+
+            assert_eq!(actual, expected);
+            assert_eq!(s.pointer, 0);
+
+            Ok(())
+        }
+    }
+
+    mod half {
+        use crate::{
+            constant::{Half, Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Half>();
+
+        #[test]
+        fn stack_underflow() {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let expected = Err(ExecuteError::StackUnderflow);
+
+            let actual = s.pop::<Half>();
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = Half::MAX / 2;
+            s.bytes[0..TYPE_SIZE].copy_from_slice(&value.to_le_bytes());
+            s.pointer = TYPE_SIZE as Word;
+            let expected = value;
+
+            let actual = s.pop::<Half>()?;
+
+            assert_eq!(actual, expected);
+            assert_eq!(s.pointer, 0);
+
+            Ok(())
+        }
+    }
+
+    mod word {
+        use crate::{
+            constant::{Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Word>();
+
+        #[test]
+        fn stack_underflow() {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let expected = Err(ExecuteError::StackUnderflow);
+
+            let actual = s.pop::<Word>();
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = Word::MAX / 2;
+            s.bytes[0..TYPE_SIZE].copy_from_slice(&value.to_le_bytes());
+            s.pointer = TYPE_SIZE as Word;
+            let expected = value;
+
+            let actual = s.pop::<Word>()?;
+
+            assert_eq!(actual, expected);
+            assert_eq!(s.pointer, 0);
+
+            Ok(())
+        }
+    }
+}
+
+#[expect(
+    clippy::indexing_slicing,
+    reason = "it is necessary to index directly in the test cases"
+)]
+#[cfg(test)]
+mod slice {
+    mod byte {
+        use crate::{
+            constant::{Byte, Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Byte>();
+
+        #[test]
+        fn stack_underflow() {
+            let s = Stack::new(TEST_STACK_SIZE);
+            let expected = Err(ExecuteError::StackUnderflow);
+
+            let actual = s.slice::<Byte>(1);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = [(Byte::MAX / 2).to_le_bytes(), (Byte::MAX / 2).to_le_bytes()]
+                .as_flattened()
+                .to_vec();
+            s.bytes[0..TYPE_SIZE * 2].copy_from_slice(&value);
+            s.pointer = TYPE_SIZE as Word * 2;
+            let expected = &[Byte::MAX / 2, Byte::MAX / 2];
+
+            let actual = s.slice::<Byte>(2)?;
+
+            assert_eq!(*actual, *expected);
+            assert_eq!(s.pointer, TYPE_SIZE as Word * 2);
+
+            Ok(())
+        }
+    }
+
+    mod quarter {
+        use crate::{
+            constant::{Quarter, Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Quarter>();
+
+        #[test]
+        fn stack_underflow() {
+            let s = Stack::new(TEST_STACK_SIZE);
+            let expected = Err(ExecuteError::StackUnderflow);
+
+            let actual = s.slice::<Quarter>(1);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = [
+                (Quarter::MAX / 2).to_le_bytes(),
+                (Quarter::MAX / 2).to_le_bytes(),
+            ]
+            .as_flattened()
+            .to_vec();
+            s.bytes[0..TYPE_SIZE * 2].copy_from_slice(&value);
+            s.pointer = TYPE_SIZE as Word * 2;
+            let expected = &[Quarter::MAX / 2, Quarter::MAX / 2];
+
+            let actual = s.slice::<Quarter>(2)?;
+
+            assert_eq!(*actual, *expected);
+            assert_eq!(s.pointer, TYPE_SIZE as Word * 2);
+
+            Ok(())
+        }
+    }
+
+    mod half {
+        use crate::{
+            constant::{Half, Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Half>();
+
+        #[test]
+        fn stack_underflow() {
+            let s = Stack::new(TEST_STACK_SIZE);
+            let expected = Err(ExecuteError::StackUnderflow);
+
+            let actual = s.slice::<Half>(1);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = [(Half::MAX / 2).to_le_bytes(), (Half::MAX / 2).to_le_bytes()]
+                .as_flattened()
+                .to_vec();
+            s.bytes[0..TYPE_SIZE * 2].copy_from_slice(&value);
+            s.pointer = TYPE_SIZE as Word * 2;
+            let expected = &[Half::MAX / 2, Half::MAX / 2];
+
+            let actual = s.slice::<Half>(2)?;
+
+            assert_eq!(*actual, *expected);
+            assert_eq!(s.pointer, TYPE_SIZE as Word * 2);
+
+            Ok(())
+        }
+    }
+
+    mod word {
+        use crate::{
+            constant::{Word, TEST_STACK_SIZE},
+            error::ExecuteError,
+            stack::Stack,
+        };
+
+        const TYPE_SIZE: usize = size_of::<Word>();
+
+        #[test]
+        fn stack_underflow() {
+            let s = Stack::new(TEST_STACK_SIZE);
+            let expected = Err(ExecuteError::StackUnderflow);
+
+            let actual = s.slice::<Word>(1);
+
+            assert_eq!(actual, expected);
+        }
+
+        #[test]
+        fn different_byte_values() -> Result<(), ExecuteError> {
+            let mut s = Stack::new(TEST_STACK_SIZE);
+            let value = [(Word::MAX / 2).to_le_bytes(), (Word::MAX / 2).to_le_bytes()]
+                .as_flattened()
+                .to_vec();
+            s.bytes[0..TYPE_SIZE * 2].copy_from_slice(&value);
+            s.pointer = TYPE_SIZE as Word * 2;
+            let expected = &[Word::MAX / 2, Word::MAX / 2];
+
+            let actual = s.slice::<Word>(2)?;
+
+            assert_eq!(*actual, *expected);
+            assert_eq!(s.pointer, TYPE_SIZE as Word * 2);
+
+            Ok(())
+        }
     }
 }
